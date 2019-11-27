0be8d22bb49290312fd2fdd587a5a0de184c34c9
Henrique Pougy <41967884+h-pgy@users.noreply.github.com>
2019-11-27 19:49:36 -0300

    prototipo inicial

---
+++ temp.py
@@ -0,0 +1,91 @@
+maping_cols = dict(
+                blocos_pavimentos_unidades = [
+                '(b)locos / \n(p)avimentos / \n(u)nidades', 
+                '(b)locos / \n(p)avimentos / \n(u)nidades',
+                '(b)locos / (p)avimentos / (u)nidades'
+    ], 
+               alvara = [
+                'alvara', 
+                'alvará'
+               ], 
+               subprefeitura = [
+                'administracao regional', 
+                'subprefeitura', 
+                'prefeitura regional',
+                'administração regional'
+               ], 
+               autor_proj = [
+                'autor do projeto', 
+                'autor projeto'
+               ], 
+               dt_aprovacao = 
+               ['aprovacao', 
+                'aprovação',
+               'dt_aprovacao'  
+               ], 
+    
+               dt_autuacao = 
+               [
+                   'dtautuacao',
+                   'data autuação'
+               ], 
+               area_terreno = 
+               [
+                   'área do terreno (m²)', 
+                'área do terreno (m²)',
+                   'area do terreno'
+               ],
+                area_constru = [
+                    'area da construcao',
+                    'área da construção (m²)'
+                ],
+                tipo_constru = [
+                     'tipo da construcao'
+                ],
+               proj_sehab = [
+                'relação dos projetos aprovados por sehab/aprov - anual 2012',
+                'relação dos projetos aprovados por sehab/aprov - anual 2011 ( sem janeiro e fevereiro)', 
+                'relação dos processos aprovados por sehab/aprov - anual 2010',
+                'relação dos processos aprovados por sehab/aprov - anual 2009',
+                'relação dos processos aprovados por sehab/aprov- anual 2008',
+                'relação dos processos aprovados por sehab/aprov - anual 2007',
+                'relação dos processos aprovados por sehba/aprov - anual 2006',
+                'relação dos processos aprovados por sehab/aprov - anual 2005',
+                'dados dos projetos aprovados por sehab - aprov -  anual 2004',
+                'dados dos projetos aprovados por sehab/aprov - anual 2002 (abril a dezembro)'
+                ],
+    
+                endereco = [
+                    'endereco',
+                    'endereço'
+                ],
+                num_endereco = [
+                    'numero'
+                ],
+                mes = [
+                    'mês',
+
+                ],
+                sql = [
+                    'sql/incra',
+                    'sql_incra'
+                ],
+                categoria_uso = [
+                'categoria de uso'
+                ],
+                proc_num = [
+                    'processo'
+                ],
+                zoneamento = [
+                    'zona de uso',
+                    'zona de uso atual'
+                ],
+                desc_alvara = [
+                    'descrição',
+                    'descricao'
+                ],
+                proprietario = [
+                    'proprietário',
+                    'proprietario'
+                ]
+)
